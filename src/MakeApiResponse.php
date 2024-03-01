6b87e94d59b9995119099ff9374149ea296f98d1
eliseekn <eliseekn@gmail.com>
2024-03-01 09:39:12 +0000

    Fix array message response

--- src/MakeApiResponse.php
+++ src/MakeApiResponse.php
@@ -16,11 +16,19 @@ trait MakeApiResponse
 
     public function successResponse(array|string $message, int $statusCode = 200): JsonResponse
     {
+        if (is_array($message)) {
+            return response()->json(array_merge(['status' => 'success'], $message), $statusCode);
+        }
+
         return $this->response('success', $message, $statusCode);
     }
 
     public function errorResponse(array|string $message, int $statusCode = 500): JsonResponse
     {
+        if (is_array($message)) {
+            return response()->json(array_merge(['status' => 'error'], $message), $statusCode);
+        }
+
         return $this->response('error', $message, $statusCode);
     }
 }
