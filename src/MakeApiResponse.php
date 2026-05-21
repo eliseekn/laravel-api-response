48da58ea159f4a24ff285534cc3284ea3707df96
eliseekn <eliseekn@gmail.com>
2026-05-21 12:44:26 +0000

    refactor: methods

--- src/MakeApiResponse.php
+++ src/MakeApiResponse.php
@@ -7,29 +7,32 @@ use Illuminate\Http\JsonResponse;
 
 trait MakeApiResponse
 {
-    public function response(string $status, array|string $message, int $statusCode): JsonResponse
+    public function jsonResponse(array $message, int $statusCode): JsonResponse
     {
-        return new JsonResponse([
-            'status' => $status,
-            'message' => $message,
-        ], $statusCode);
+        return new JsonResponse($message, $statusCode);
     }
 
-    public function successResponse(array|string $message, int $statusCode = 200): JsonResponse
+    public function successJsonResponse(array|string $message, int $statusCode = 200): JsonResponse
     {
         if (is_array($message)) {
             return new JsonResponse(array_merge(['status' => HttpResponseStatus::SUCCESS], $message), $statusCode);
         }
 
-        return $this->response(HttpResponseStatus::SUCCESS, $message, $statusCode);
+        return new JsonResponse([
+            'status' => HttpResponseStatus::SUCCESS,
+            'message' => $message,
+        ], $statusCode);
     }
 
-    public function errorResponse(array|string $message, int $statusCode = 500): JsonResponse
+    public function errorJsonResponse(array|string $message, int $statusCode = 500): JsonResponse
     {
         if (is_array($message)) {
             return new JsonResponse(array_merge(['status' => HttpResponseStatus::ERROR], $message), $statusCode);
         }
 
-        return $this->response(HttpResponseStatus::ERROR, $message, $statusCode);
+        return new JsonResponse([
+            'status' => HttpResponseStatus::ERROR,
+            'message' => $message,
+        ], $statusCode);
     }
 }
